2284c8832cc0bacc0963733804f54b2f0b1968a7
samyfrey <samyfrey@yahoo.com>
2022-03-28 15:14:08 -0400

    added client create first commit

--- client/src/components/pages/Clients/ClientCreate.jsx
+++ client/src/components/pages/Clients/ClientCreate.jsx
@@ -3,31 +3,33 @@ import { createClient } from '../../../api/clients'
 import '../../styles/Client.scss'
 
 export const ClientCreate = ({ user }) => {
-  const [client, setClient] = useState('')
+  const [name, setName] = useState('')
+  const [industry, setIndustry] = useState('')
   // post axios request
   // form
 
   const handleCreate = async event => {
     event.preventDefault()
     try {
-      const res = await createClient(user, client)
-      setClient(res.data.client)
+      const res = await createClient(user, name, industry)
+      setName(res.data.client.name)
+      setIndustry(res.data.client.industry)
     } catch (error) {
       console.log(error)
     }
   }
   return (
 
-    <form className='create-client-form' onSubmit={{ handleCreate }}>
+    <form className='create-client-form' onSubmit={ handleCreate}>
       <div className="create-client-item">
         <label>Company Name</label>
-        <input type='text' placeholder='Type Company Name' value={client.name}/>
+        <input type='text' placeholder='Type Company Name' value={name} onChange={event => setName(event.target.value)}/>
       </div>
       <div className="create-client-item">
         <label>Industry</label>
-        <input type='text' placeholder='Type Company Industry' value={client.industry}/>
+        <input type='text' placeholder='Type Company Industry' value={industry} onChange={event => setIndustry(event.target.value)}/>
       </div>
-      <button className="create-client-btn">Create</button>
+      <button className="create-client-btn" type="submit">Create</button>
 
     </form>
 
